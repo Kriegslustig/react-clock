2710d24cd1582e29491f958fcb7b7c53ea1249d5
Kriegslustig <github@ls7.ch>
2016-03-16 11:32:04 +0100

    Make the clock prettier and move some stuff from view to model

--- src/clock.jsx
+++ src/clock.jsx
@@ -4,13 +4,13 @@ require('./clock.css')
 module.exports = props => (
   <div className="clock">
     <div className="clock__hand clock__hand--hours" style={{
-      transform: `rotate(${props.hours * 360 / 12}deg)`
+      transform: `rotate(${props.hours}deg)`
     }}></div>
     <div className="clock__hand--minutes clock__hand" style={{
-      transform: `rotate(${props.minutes * 360 / 60}deg)`
+      transform: `rotate(${props.minutes}deg)`
     }}></div>
     <div className="clock__hand--seconds clock__hand" style={{
-      transform: `rotate(${props.seconds * 360 / 60}deg)`
+      transform: `rotate(${props.seconds}deg)`
     }}></div>
   </div>
 )
--- src/time.jsx
+++ src/time.jsx
@@ -1,6 +1,6 @@
 module.exports = {
-  hours: time => Math.round((time || new Date()).getTime() / 1000 / 60 / 60 % 12),
-  minutes: time => Math.round((time || new Date()).getTime() / 1000 / 60 % 60),
-  seconds: time => Math.round((time || new Date()).getTime() / 1000 % 60)
+  hours: time => (time || new Date()).getTime() / 1000 / 60 / 60 % 12 * 360 / 12,
+  minutes: time => (time || new Date()).getTime() / 1000 / 60 % 60 * 360 / 60,
+  seconds: time => Math.round((time || new Date()).getTime() / 1000 % 60) * 360 / 60
 }
 
